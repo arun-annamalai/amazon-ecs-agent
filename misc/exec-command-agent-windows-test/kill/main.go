0cc5caa7c84a70ac7ce19bdb93fcdc03968d7f45
Arun Annamalai <aruanna@amazon.com>
2021-06-24 10:40:09 -0700

    Create execcmd windows test infrastructure

---
+++ misc/exec-command-agent-windows-test/kill/main.go
@@ -0,0 +1,39 @@
+// +build windows,integration
+
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+func main() {
+	var pidFlag = flag.Int("pid", -1, "PID of the process to kill")
+	flag.Parse()
+	if *pidFlag == -1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	//p, _ := os.FindProcess(*pidFlag)
+	//p.Kill()
+
+	kill := exec.Command("TASKKILL", "/T", "/F", "/PID", strconv.Itoa(*pidFlag))
+	kill.Stderr = os.Stderr
+	kill.Stdout = os.Stdout
+	kill.Run()
+}
