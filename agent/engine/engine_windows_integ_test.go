cf7e4498e2fc4639ef626ed64059022ff21b803a
Arun Annamalai <aruanna@amazon.com>
2021-06-28 22:15:04 -0700

    Update engine_windows_integ_test.go

--- agent/engine/engine_windows_integ_test.go
+++ agent/engine/engine_windows_integ_test.go
@@ -882,7 +882,7 @@ func killMockExecCommandAgent(t *testing.T, client *sdkClient.Client, containerI
 	})
 	require.NoError(t, err)
 
-	time.Sleep(200000)
+	time.Sleep(20000 * time.Second)
 	top, err := client.ContainerTop(ctx, containerId, nil)
 	seelog.Infof("LOOK HERE1")
 	seelog.Infof("LOOK HERE1")
